fff5a80c6343897c9901d3490e32fb2ab9ba6a3d
Jeremy Malais <contact@jmalais.com>
2019-08-12 00:12:04 -0600

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,16 +1,14 @@
-// Ping-Pong
-module.exports = (client, message) => {
-  if (message.content === 'ping') {
-    message.reply('Pong!')
-  }
-});
-
-
-// Kick Message
+// Include: kick
 const kick = require("../commands/kick");
 
 module.exports = (client, message) => {
+  // Kick Message
   if (message.content.startsWith("/c kick")) {
     return kick(message);
+  }
+
+  // Ping-Pong
+  if (message.content === 'ping') {
+    message.reply('Pong!')
   }
 };
